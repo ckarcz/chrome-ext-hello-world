1552e5c7fbc8a404ca660a40931a1a9e989f7df0
Chris Karcz <ckarcz@gmail.com>
2018-08-08 11:39:09 -0400

    fix typos

--- options.js
+++ options.js
@@ -4,14 +4,14 @@ let buttonsDiv = document.getElementById('buttonsDiv');
 // available color choices
 const colorChoices = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 
-// func that creats and appends color buttons to the element supplied
+// func that creates and appends color buttons to the element supplied
 function createColorButtons(element, colors) {
     for (let color of colors) {
         let chooseColorBtn = document.createElement('button');
         chooseColorBtn.style.backgroundColor = color;
         chooseColorBtn.addEventListener('click', function () {
             chrome.storage.sync.set({ color: color }, function () {
-                console.log('color is ' + color);
+                console.log('color set to ' + color);
             })
         });
         element.appendChild(chooseColorBtn);
